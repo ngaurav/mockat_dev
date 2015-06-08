dab2f0b02204fe1f5db3d5fcc8b45252180bc328
Nishant Gaurav <nishantgaurav@icloud.com>
2015-06-08 04:21:56 +0000

    v1.9.1

--- myproject/quiz/category_context_processor.py
+++ myproject/quiz/category_context_processor.py
@@ -1,11 +1,7 @@
 from quiz.models import Category, Domain
-from andablog.models import Entry
 
 def category_list(request):
     return {'category_list' : Category.objects.all().order_by('rank')}
 
 def domain_list(request):
     return {'domain_list' : Domain.objects.all().order_by('rank')}
-
-def entry_list(request):
-    return {'entry_list': Entry.objects.all().order_by('rank')}
