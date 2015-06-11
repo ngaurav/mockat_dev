fa2c47780a3480d1a4c86bc63ee32bf17981d6a8
Nishant Gaurav <nishantgaurav@icloud.com>
2015-06-11 04:08:45 +0000

    v2.0.6

--- myproject/allaccess/views.py
+++ myproject/allaccess/views.py
@@ -39,6 +39,14 @@ class OAuthRedirect(OAuthClientMixin, RedirectView):
     permanent = False
 
     def get_additional_parameters(self, provider):
+        if provider.name == 'facebook':
+            # Request permission to see user's email
+            return {'scope': 'email'}
+        if provider.name == 'google':
+            # Request permission to see user's profile and email
+            perms = ['userinfo.email', 'userinfo.profile']
+            scope = ' '.join(['https://www.googleapis.com/auth/' + p for p in perms])
+            return {'scope': scope}
         "Return additional redirect parameters for this provider."
         return {}
 
