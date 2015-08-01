466211ab5aadb755dffcf4e0995cab33ef39f922
Nishant Gaurav <nishantgaurav@icloud.com>
2015-08-01 09:49:10 +0000

    v4.99

--- myproject/quiz/models.py
+++ myproject/quiz/models.py
@@ -711,7 +711,7 @@ class Question(models.Model):
         ('SA', 'ShortAnswer'),
     )
 
-    ques_type = models.CharField(max_length=2,choices=QUESTION_TYPES,default='MCQ')
+    ques_type = models.CharField(max_length=1,choices=QUESTION_TYPES,default='MCQ')
 
     section_two = models.BooleanField(default = False, blank=False,
         help_text=_("Verbal Ability Logical Reasoning section"),
